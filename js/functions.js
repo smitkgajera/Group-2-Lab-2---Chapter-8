d92359a171145cefb74849f098f4b56f5ba23a0b
smitkgajera <59576836+smitkgajera@users.noreply.github.com>
2020-11-07 23:14:18 -0500

    Created Function
    Function for calculating subtotal

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,21 @@
 /* define functions here */
 
+var total = 0;
+var subTotal = 0;
+var shipping = 0;
+var tax = 0;
+var grandTotal = 0;
+var filenames = ["images/106020.jpg", "images/116010.jpg", "images/120010.jpg"];
+var titles = ["Girl with a Pearl Earring", "Artist Holding a Thistle", "Portrait of Eleanor of Toledo"];
+var quantities = [3, 1, 2];
+var prices = [80, 125, 75];
 
+function calculateTotal(x, y){
+    subTotal = x*y;
+    total += subTotal; 
+    return subTotal;
+}
+function calculateFinalTotal(){
+    return total;
+}
         
