b823a4eb86fb0e0d3cfa73be97c983d727de5df5
danny9996 <59576826+danny9996@users.noreply.github.com>
2020-11-08 00:12:40 -0500

    Updated Js

--- js/chapter08-project01.js
+++ js/chapter08-project01.js
@@ -1,5 +1,11 @@
 /* add loop and other code here ... in this simple exercise we are not
    going to concern ourselves with minimizing globals, etc */
+   var title = $('<h1>Shopping Cart</h1>');
 
+   $(function(){
+      $(".title").empty();
+      $(".title").append(title); // just replacing title
+      }   
+   );
 
 
