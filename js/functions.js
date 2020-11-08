d72e5b3c00b1eebd096c7987dd1828ba00d807db
uprajapa <59576793+uprajapa@users.noreply.github.com>
2020-11-07 23:09:08 -0500

    Starting joining scripts

--- js/functions.js
+++ js/functions.js
@@ -1,29 +1,4 @@
 /* define functions here */
 
-function calculateTotal (quantities, price) {
-    return quantities * price;
-
-}
-
-fuction outputCartRow (file, title, quantity, price, total) {
-    document.write("<table border=1>")
-  for (row=1; row<=5; row++) {
-     document.write("<tr>")
-     for (col=1; col<=5; col++) {
-           document.write("<td>R" + row + "<br>C" + col + "</td>")
-      }
-      document.write("</tr>")
- }
- document.write("</table>")
-}
-
-function subtotal (x,y,z) {
-    return (x+y+z)
-
-}
-function tax() {
-    
-}
-
 
         
