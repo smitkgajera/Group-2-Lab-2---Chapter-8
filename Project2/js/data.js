74ab90e0575b5390710208aa41af7d7bc648e5e6
uprajapa <59576793+uprajapa@users.noreply.github.com>
2020-11-07 23:54:52 -0500

    Added array for furthur process

---
+++ Project2/js/data.js
@@ -0,0 +1,31 @@
+/* define your array of object literals here */
+var countries = {
+    canada:{
+    name:"Canada",
+    continent: "North America",
+    cities: ["Calgary", "Montreal", "Toronto"],
+    photos: ["images/canada1.jpg","images/canada2.jpg","images/canada3.jpg"]},
+    us:{
+        name:"United States",
+        continent: "North America",
+        cities: ["Boston", "Chicago", "New York", "Seattle", "Washington"],
+        photos: ["images/us1.jpg","images/us2.jpg"]
+    } ,
+    italy:{
+        name:"Italy",
+        continent: "Europe",
+        cities: ["Florence", "Milan", "Naples","Rome"],
+        photos: ["images/italy1.jpg","images/italy2.jpg","images/italy3.jpg","images/italy4.jpg","images/italy5.jpg","images/italy6.jpg"]
+    },
+    spain:{
+        name:"Spain",
+        continent: "Europe",
+        cities: ["Almeria", "Barcelona", "Madrid"],
+        photos: ["images/spain1.jpg","images/spain2.jpg"]
+    }
+}
+
+$(function(){
+
+})
+  
